ce9db0f3eab77ca0cdba422f2b58ac738590a7a5
Andrea Nonali <nonali.andrea@gmail.com>
2022-09-29 12:43:13 +0200

    justify text content

--- pages/index.js
+++ pages/index.js
@@ -95,6 +95,8 @@ export default function Home() {
         <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
           <h1 className="tablet:m-10 text-2xl text-bold">About me</h1>
           <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
+          <h1 className="text-4xl text-bold">About me</h1>
+          <p className="text-justify mt-2 text-xl laptop:text-xl w-full laptop:w-3/5 text-bold">
             {data.aboutpara}
           </p>
         </div>
